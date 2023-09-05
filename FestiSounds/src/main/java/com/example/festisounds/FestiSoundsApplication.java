23a09e8db64aaa636e85747d02e35b3b0b436ae8
Robbie Corcoran <codewithrobbie@gmail.com>
2023-09-05 09:56:38 +0200

    Project Initialized

---
+++ FestiSounds/src/main/java/com/example/festisounds/FestiSoundsApplication.java
@@ -0,0 +1,13 @@
+package com.example.festisounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiSoundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestiSoundsApplication.class, args);
+	}
+
+}
