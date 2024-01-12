52e2743e0d2e331b3d5b896eace5e3bb0e5e0d69
Max Burrows <maxajburrows@gmail.com>
2024-01-12 17:12:22 +0100

    Pass all paramitarised tests for getArtistScore> > Co-authored-by: Iliana Scalco <dokratos@hotmail.it>

--- FestiSounds/src/test/java/com/example/festisounds/Modules/UserData/Services/UserArtistMatchingServiceImplTest.java
+++ FestiSounds/src/test/java/com/example/festisounds/Modules/UserData/Services/UserArtistMatchingServiceImplTest.java
@@ -17,9 +17,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserArtistMatchingServiceImplTest {
 
-
-
-
     @ParameterizedTest
     @MethodSource("artistScoreParameters")
     void testGetArtistScore_WhenProvidedValidScores_ReturnsCorrectResult(Double expectedResult, ArrayList<Double> genreScores) {
@@ -30,12 +27,11 @@ class UserArtistMatchingServiceImplTest {
                 "Artist score of " + actualResult + " was returned instead of " + expectedResult);
     }
     public static Stream<Arguments> artistScoreParameters() {
-
         return Stream.of(
-                Arguments.of(96.0, new ArrayList<>(Arrays.asList(90.0, 80.0, 70.0))),
+                Arguments.of(99.4, new ArrayList<>(Arrays.asList(90.0, 80.0, 70.0))),
                 Arguments.of(70.25, new ArrayList<>(Arrays.asList(50.0, 30.0, 15.0))),
-                Arguments.of(93.0, new ArrayList<>(Arrays.asList(87.4, 34.8, 93))),
-                Arguments.of(96.0, new ArrayList<>(Arrays.asList(93.0, 2.0))),
+                Arguments.of(93.07, new ArrayList<>(Arrays.asList(87.4, 34.8, 15.6))),
+                Arguments.of(93.14, new ArrayList<>(Arrays.asList(93.0, 2.0))),
                 Arguments.of(4.0, new ArrayList<>(List.of(4.0)))
                 );
     }
