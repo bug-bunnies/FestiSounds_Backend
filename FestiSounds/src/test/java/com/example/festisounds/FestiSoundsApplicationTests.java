a137254cdb2828f4bf25e540105f599c94b7884e
Max Burrows <maxajburrows@gmail.com>
2024-01-19 16:39:06 +0100

    Try to get application context to load during tests > > Co-authored-by: Iliana Scalco <dokratos@hotmail.it>

--- FestiSounds/src/test/java/com/example/festisounds/FestiSoundsApplicationTests.java
+++ FestiSounds/src/test/java/com/example/festisounds/FestiSoundsApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.festisounds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+//@ContextConfiguration(classes = FestiSoundsApplication.class)
+
 class FestiSoundsApplicationTests {
 
 	@Test
