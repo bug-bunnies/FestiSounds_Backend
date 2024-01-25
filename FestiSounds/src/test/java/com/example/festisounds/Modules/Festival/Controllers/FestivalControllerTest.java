5db2380f7c2442a62ad7510d94ecc5ef77e75164
Robbie Corcoran <codewithrobbie@gmail.com>
2024-01-25 15:33:39 +0100

    Began FestivalControllerTest

---
+++ FestiSounds/src/test/java/com/example/festisounds/Modules/Festival/Controllers/FestivalControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.festisounds.Modules.Festival.Controllers;
+
+import com.example.festisounds.Modules.Festival.Controller.FestivalController;
+import com.example.festisounds.Modules.Festival.DTO.FestivalRequestDTO;
+import com.example.festisounds.Modules.Festival.DTO.FestivalResponseDTO;
+import com.example.festisounds.Modules.Festival.Service.FestivalService;
+import com.example.festisounds.Modules.FestivalArtists.DTO.ArtistResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = FestivalController.class)
+@MockBean({FestivalService.class})
+public class FestivalControllerTest {
+
+
+
+}
