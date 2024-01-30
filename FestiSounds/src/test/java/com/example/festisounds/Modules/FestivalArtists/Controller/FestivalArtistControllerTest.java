e21e5d1edf832ddb72fa7b83061ced0677c13c6a
dokratos <dokratos@hotmail.it>
2024-01-30 15:28:50 +0100

    this controller is a maze

--- FestiSounds/src/test/java/com/example/festisounds/Modules/FestivalArtists/Controller/FestivalArtistControllerTest.java
+++ FestiSounds/src/test/java/com/example/festisounds/Modules/FestivalArtists/Controller/FestivalArtistControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -20,6 +21,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = ArtistController.class)
@@ -31,22 +33,27 @@ public class FestivalArtistControllerTest {
     @MockBean
     ArtistService service;
 
-    UUID festivalUUID = UUID.randomUUID();
+    UUID festivalUUID1;
+    UUID festivalUUID2;
     ArtistResponseDTO artistResponseDTO;
 
     @BeforeEach
     void setup() {
+        festivalUUID1 = UUID.randomUUID();
+        festivalUUID2 = UUID.randomUUID();
+
         artistResponseDTO = new ArtistResponseDTO(UUID.randomUUID(),
                 "ArtistSpotifyId",
                 "ArtistName",
                 Set.of("Genre1", "Genre2"),
-                Set.of(festivalUUID));
+                Set.of(festivalUUID1));
     }
 
+//    TODO: test request to spotify?!?
     @Test
     @DisplayName("Find Artist by name in Spotify")
     void testGetAllArtist_whenGivenASearchParam_returnsArrayOfArtists() {
-
+        fail("Test must be implemented yet!");
     }
 
     @Test
@@ -67,5 +74,37 @@ public class FestivalArtistControllerTest {
                 () -> "The returned artist name is incorrect");
         assertEquals(artistResponseDTO.spotifyId(), foundArtist.spotifyId(),
                 () -> "The returned spotifyId is incorrect");
+    }
+
+    @Test
+    @DisplayName("Get Artist by Id returns 404 when not found")
+    void testFindArtist_whenGivenNotExistingID_returns404() throws Exception {
+        UUID id = UUID.randomUUID();
+        when(service.getArtist(id)).thenThrow();
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/artists/" + id);
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), mvcResult.getResponse().getStatus(),
+                () -> "Incorrect Response Status");
+    }
+
+    @Test
+    @DisplayName("Artist can be created")
+    void testCreateArtist_whenGivenNewArtistName_createAndAddArtistToFestival() {
+        ArtistResponseDTO artistResponseDTO = new ArtistResponseDTO(UUID.randomUUID(),
+                "SpotifyId",
+                "test1",
+                Set.of("Genre3", "Genre2"),
+                Set.of(festivalUUID1));
+
+
+
+    }
+
+    @Test
+    @DisplayName("Artist can be added to festival")
+    void testCreateArtist_whenGivenExistingArtist_addArtistToFestival() {
+
     }
 }
